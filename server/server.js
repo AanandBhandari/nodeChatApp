e1c843caf60432c66f3f6ef67d5b570281e13c12
AanandBhandari <aanandbhandari143@gmail.com>
2018-04-12 22:13:02 +0545

    add npm scripts/start

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
+var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000,()=> {
-    console.log('Server is running now on port 3000');
+app.listen(port,()=> {
+    console.log(`Server is up on port ${port}`);
 });
