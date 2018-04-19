aa9f9a25ce19c38eef5191345e9cb61dd0afb179
AanandBhandari <aanandbhandari143@gmail.com>
2018-04-19 16:18:08 +0545

    change some style

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
         users.removeUser(socket.id); 
         users.addUser(socket.id,params.name,params.room);
         io.to(params.room).emit('updateUserList',users.getUserList(params.room));
-        socket.emit('newMessage',generateMessage('Aanand','Welcome to my Chat App'));
+        socket.emit('newMessage',generateMessage('Root','Welcome to my Chat App'));
     socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
         callback();
     });
